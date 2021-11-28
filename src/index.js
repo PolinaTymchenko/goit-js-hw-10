5d73f354c390f4213d603ea390614f80e63d191a
Polina Tymchenko <polina0503800814@gmail.com>
2021-11-28 21:57:00 +0200

    export of css files was changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+// import './css/styles.css';
 import { fetchCountries } from "./fetchCountries.js";
 import Notiflix from 'notiflix';
 import countryCard from "./country-card.hbs";
